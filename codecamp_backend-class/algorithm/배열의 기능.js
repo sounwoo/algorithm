62352ac651c53d91a0d7b7910a445617b788f046
sounwoo <mna800@naver.com>
2023-01-09 14:22:33 +0900

    algorithm 2일차

---
+++ codecamp_backend-class/algorithm/배열의 기능.js
@@ -0,0 +1,20 @@
+// **`문제 설명`**
+// 주어진 fruits 배열에서 마지막 요소를 꺼내 newFruits에 넣어주세요.
+
+// **`입력 인자`**
+// - X
+
+// **`주의 사항`**
+// - length를 이용해서 문제를 풀어야 합니다.
+// - push를 이용해서 문제를 풀어야 합니다.
+// - 마지막 요소를 꺼낼 때 length를 사용하는 이유무엇일까요.
+
+// **`예상 결과`**
+// console.log(newFruits) // ["파인애플"]
+
+let fruits = ['사과', '바나나', '파인애플'];
+let newFruits = [];
+
+newFruits.push(fruits[fruits.length - 1]);
+
+console.log(newFruits);
