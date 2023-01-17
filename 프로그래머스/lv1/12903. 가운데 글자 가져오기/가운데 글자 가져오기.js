c3ae6d8a43655f2e309d01bc300a4333da2253ee
선우 <105111888+sounwoo@users.noreply.github.com>
2023-01-17 10:25:51 +0900

    [level 1] Title: 가운데 글자 가져오기, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv1/12903. 가운데 글자 가져오기/가운데 글자 가져오기.js
+++ 프로그래머스/lv1/12903. 가운데 글자 가져오기/가운데 글자 가져오기.js
@@ -4,6 +4,7 @@ function solution(s) {
     const center = Math.floor(s.length /2)
     // s.length % 2 나눠서 짝수인지 판별
     // s[center - 1] index 번호를 -1 해주면 1번째 글자를 가져오게 된다.
+    // s.substring(center - 1, center + 1) 글자를 잘라오는 것도 가능
     answer = s.length % 2 === 0 ? s[center - 1] + s[center] : s[center];
     
     
