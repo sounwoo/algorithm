67977ac322df9e1323d68de5915dfd2b65b843e0
선우 <105111888+sounwoo@users.noreply.github.com>
2023-01-17 10:39:37 +0900

    [level 1] Title: 서울에서 김서방 찾기, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv1/12919. 서울에서 김서방 찾기/서울에서 김서방 찾기.js
+++ 프로그래머스/lv1/12919. 서울에서 김서방 찾기/서울에서 김서방 찾기.js
@@ -1,10 +1,13 @@
 function solution(seoul) {
     var answer = '';
-    for(let i=0; i<=seoul.length; i++){
-        if(seoul[i] === "Kim") { // seoul의 i번째 값이 Kim 이면
-            answer = `김서방은 ${i}에 있다`;
-            break;
-        }
-    }
+    // .indexOf("Kim")을 찾는다.
+    const i = seoul.indexOf("Kim")
+    answer = `김서방은 ${i}에 있다`
+    // for(let i=0; i<=seoul.length; i++){
+    //     if(seoul[i] === "Kim") { // seoul의 i번째 값이 Kim 이면
+    //         answer = `김서방은 ${i}에 있다`;
+    //         break;
+    //     }
+    // }
     return answer;
 }
