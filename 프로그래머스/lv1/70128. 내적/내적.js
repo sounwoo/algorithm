64441639d0d48a6d89bdcb337b2b88ae1830dded
선우 <105111888+sounwoo@users.noreply.github.com>
2023-01-30 10:14:07 +0900

    [level 1] Title: 내적, Time: 0.08 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv1/70128. 내적/내적.js
+++ 프로그래머스/lv1/70128. 내적/내적.js
@@ -1,8 +1,13 @@
 function solution(a, b) {
-    var answer = 0;
+    const answer = a.reduce((acc,cur,i)=>{
+       return acc + (cur * b[i])
+    },0)
+    return answer
     
-    for(let i = 0; i < a.length; i++){ 
-        answer += a[i]*b[i];
-    }
-    return answer;
+//     var answer = 0;
+    
+//     for(let i = 0; i < a.length; i++){ 
+//         answer += a[i]*b[i];
+//     }
+//     return answer;
 }
