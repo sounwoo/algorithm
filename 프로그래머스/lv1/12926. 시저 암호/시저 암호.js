57979ee6942b525e23857dd654b6d881a5effcd3
선우 <105111888+sounwoo@users.noreply.github.com>
2023-02-08 10:31:19 +0900

    [level 1] Title: 시저 암호, Time: 23.95 ms, Memory: 36.7 MB -BaekjoonHub

--- 프로그래머스/lv1/12926. 시저 암호/시저 암호.js
+++ 프로그래머스/lv1/12926. 시저 암호/시저 암호.js
@@ -1,20 +1,54 @@
-function solution(s, n) {
-    const upper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-    const lower = "abcdefghijklmnopqrstuvwxyz";
+
+
+function solution(s,n) {
+  const upper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  const lower = "abcdefghijklmnopqrstuvwxyz";
+  
+  const answer = s.split("").reduce((acc,cur) => {
+    const word = lower.includes( cur ) ? lower : upper;
+    let idx = word.indexOf( cur ) + n
     
-    let answer = ""
-    
-    for(let i = 0; i < s.length; i++){
-        if(s[i] == " "){
-            answer += " "
-            continue
-        }
-        
-       let arr = upper.includes(s[i]) ? upper : lower
-       let index = arr.indexOf(s[i]) + n
-       if(index >= arr.length) index -= arr.length;
-        answer += arr[index];
-    }
-    return answer
-    
+    if( idx > 25 ) idx -=26
+    return acc + (cur === " " ? " " : word[idx])
+  },"")
+
+  
+  
+  //   const upper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+//   const lower = "abcdefghijklmnopqrstuvwxyz";
+//   let answer = "";
+//   for(let i = 0; i < s.length; i++){
+//     if(s[i] === " ") answer += s[i] // 공백일 경우 s[i] = " "
+//     else{
+//       const word = lower.includes( s[i] ) ? lower : upper;
+//       let idx = word.indexOf( s[i] ) + n ;
+
+      
+//       if ( idx > 25) {
+//         idx -= 26;
+//       }
+      
+//       answer += word[idx]
+//     }
+//   }
+  
+  
+  // const alphabet = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  // let answer = "";
+  // for(let i = 0; i < s.length; i++){
+  //   if(s[i] === " ") answer += s[i] // 공백일 경우 s[i] = " "
+  //   else{
+  //     let idx = alphabet.indexOf( s[i] )
+  //     const word = idx > 25 ? alphabet.slice( 26 ) : alphabet.slice( 0,26 )
+  //     idx = word.indexOf( s[i] ) + n
+      
+  //     if( idx > 25 ){
+  //       idx -= 26
+  //     }
+      
+  //     answer += word[idx]
+  //   }
+  // }
+  
+      return answer
 }
