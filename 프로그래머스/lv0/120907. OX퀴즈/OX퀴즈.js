ef48890802e1481f1b3001423da5624fc7fdedcb
선우 <105111888+sounwoo@users.noreply.github.com>
2023-02-14 15:08:17 +0900

    [level 0] Title: OX퀴즈, Time: 0.09 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120907. OX퀴즈/OX퀴즈.js
+++ 프로그래머스/lv0/120907. OX퀴즈/OX퀴즈.js
@@ -1,20 +1,10 @@
 function solution(quiz) {
   let result = []; 
   for(let i =0; i <quiz.length; i++){
-    let word = quiz[i].split(" ")
-    if(word[1] === '-'){
-      if(Number(word[0]) - Number(word[2]) === Number(word[4])){
-        result.push("O")
-      }else{
-        result.push('X')
-      }
-    }else{
-      if(Number(word[0]) + Number(word[2]) === Number(word[4])){
-        result.push("O")
-      }else{
-        result.push('X')
-      }
-    }
+    let word = quiz[i].split("=")
+      eval(word[0]) === Number(word[1]) 
+        ? result.push("O") 
+        : result.push('X')
   }
   return result
 }
