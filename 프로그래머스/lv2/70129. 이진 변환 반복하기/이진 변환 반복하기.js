60b906658c84948c4a967503c76c0b14f4e493b7
선우 <105111888+sounwoo@users.noreply.github.com>
2023-02-15 10:53:06 +0900

    [level 2] Title: 이진 변환 반복하기, Time: 10.08 ms, Memory: 39.6 MB -BaekjoonHub

--- 프로그래머스/lv2/70129. 이진 변환 반복하기/이진 변환 반복하기.js
+++ 프로그래머스/lv2/70129. 이진 변환 반복하기/이진 변환 반복하기.js
@@ -1,11 +1,24 @@
 function solution(s) {
-  let answer = [0,0]
-    while(s !== "1"){
-      answer[0] ++
-      for(let i = 0; i < s.length; i++){
-        if(s[i] === "0") answer[1] ++
-      }
-      s = s.replaceAll(0,"").length.toString(2)
-    }
-  return answer
+    function recursion(s, [count,remove]){
+        if(s === "1") return [count,remove]
+        count++  
+        remove += s.split('').filter(el => el === '0').length;
+        s = s.split("").filter(el => el === "1").length.toString( 2 )
+    
+        return recursion(s, [count,remove])
+    
+  }
+    
+  return recursion(s, [0,0])
+    
+    
+  // let answer = [0,0]
+  //   while(s !== "1"){
+  //     answer[0] ++
+  //     for(let i = 0; i < s.length; i++){
+  //       if(s[i] === "0") answer[1] ++
+  //     }
+  //     s = s.replaceAll(0,"").length.toString(2)
+  //   }
+  // return answer
 }
