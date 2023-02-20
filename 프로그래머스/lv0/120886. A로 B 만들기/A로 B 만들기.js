4aa6022519ac2e1f8db7119bfa2a7624d6d13532
선우 <105111888+sounwoo@users.noreply.github.com>
2023-02-20 10:50:57 +0900

    [level 0] Title: A로 B 만들기, Time: 0.03 ms, Memory: 33.1 MB -BaekjoonHub

--- 프로그래머스/lv0/120886. A로 B 만들기/A로 B 만들기.js
+++ 프로그래머스/lv0/120886. A로 B 만들기/A로 B 만들기.js
@@ -1,12 +1,3 @@
 function solution(before, after) {
-  let obj = {} , obj1 = {} 
-  for(let i = 0; i < before.length; i++){
-      obj[before[i]] = (obj[before[i]] || 0) +1
-      obj1[after[i]] = (obj1[after[i]] || 0) +1
-    }
-
-  for(let i in obj){
-    if (obj[i] !== obj1[i]) return 0
-  }
-  return 1
+  return before.split("").sort().join('') === after.split("").sort().join('') ? 1 : 0
 }
