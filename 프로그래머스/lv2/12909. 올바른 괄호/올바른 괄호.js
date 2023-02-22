f24fde7f34d2c57628aeaf1d90864a8e2578e769
선우 <105111888+sounwoo@users.noreply.github.com>
2023-02-22 15:09:11 +0900

    [level 2] Title: 올바른 괄호, Time: 5.58 ms, Memory: 38.3 MB -BaekjoonHub

--- 프로그래머스/lv2/12909. 올바른 괄호/올바른 괄호.js
+++ 프로그래머스/lv2/12909. 올바른 괄호/올바른 괄호.js
@@ -1,7 +1,7 @@
 function solution(s){
    let count = 0
-   if(s.slice(-1) === '(') return false
-   if(s.slice(1) === ')') return false
+   if(s.slice(-1) === '(' || s.slice(1) === ')') return false
+
    for(let i of s){
       i === "("? count ++: count --     
      if(count< 0) return false
