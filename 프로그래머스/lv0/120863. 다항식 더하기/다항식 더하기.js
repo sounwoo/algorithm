2e6f9ef89eeb0145866cec17dac3bf68d6c683fd
선우 <105111888+sounwoo@users.noreply.github.com>
2023-02-24 09:32:33 +0900

    [level 0] Title: 다항식 더하기, Time: 0.10 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120863. 다항식 더하기/다항식 더하기.js
+++ 프로그래머스/lv0/120863. 다항식 더하기/다항식 더하기.js
@@ -1,11 +1,8 @@
 function solution(polynomial) {
   let result = 0, num = 0;
     polynomial.replaceAll(" +", "").split(" ").forEach((el) =>{
-      if(el.includes("x")){
-        if(el.replaceAll("x","") === "")result +=1
-        else result += Number(el.split("x")[0])
-      }else num += Number(el)
-
+      if(el.includes("x"))result += Number(el.replace("x","")||1);
+      else num += Number(el)
     })
   if(result === 1) result = ""
   if(result === 0) return String(num)
