becb2fa75c98ca182a2989677bb97134cd42d5c6
선우 <105111888+sounwoo@users.noreply.github.com>
2023-02-27 12:44:25 +0900

    [level 0] Title: 배열 회전시키기, Time: 0.03 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120844. 배열 회전시키기/배열 회전시키기.js
+++ 프로그래머스/lv0/120844. 배열 회전시키기/배열 회전시키기.js
@@ -1,5 +1,6 @@
 function solution(numbers, direction) {
-    if(direction === "right") numbers.unshift(numbers.pop())
-    else numbers.push(numbers.shift())
+    direction === "right"
+    ?numbers.unshift(numbers.pop())
+    :numbers.push(numbers.shift())
   return numbers
 }
