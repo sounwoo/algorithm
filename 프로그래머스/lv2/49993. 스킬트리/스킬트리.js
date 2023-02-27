9f7dbbe939ff0229087d1bbaf5df9cb9ec1b6c70
선우 <105111888+sounwoo@users.noreply.github.com>
2023-02-27 10:44:18 +0900

    Update 스킬트리.js

--- 프로그래머스/lv2/49993. 스킬트리/스킬트리.js
+++ 프로그래머스/lv2/49993. 스킬트리/스킬트리.js
@@ -1,21 +1,31 @@
 function solution(skill, skill_trees) {
-    let answer = 0;
+    const answer = skill_trees.reduce((acc, cur)=> {
+      const filltered = [...cur].filter((str)=>{
+        return skill.includes(str)
+      }).join("")
+      return acc + ( (skill.includes( filltered ) && skill[0] === filltered[0]) || 
+                    filltered === '' 
+                    ? 1
+                    : 0 )
+    },0)
+    return answer
+//     let answer = 0;
   
-    for(let i =0; i < skill_trees.length; i++){
-      let filltered = "";
-      for(let j =0 ; j < skill_trees[i].length; j++){
-        if( skill.includes( skill_trees[i][j] ) ){
-          filltered += skill_trees[i][j]
-        }
-      }
-      if( filltered === "" ) filltered = skill
-      if( skill.includes( filltered ) ){
-          if( filltered[0] === skill[0] ){
-            answer++
-          }
-        }
-    }
-  return answer
+//     for(let i =0; i < skill_trees.length; i++){
+//       let filltered = "";
+//       for(let j =0 ; j < skill_trees[i].length; j++){
+//         if( skill.includes( skill_trees[i][j] ) ){
+//           filltered += skill_trees[i][j]
+//         }
+//       }
+//       if( filltered === "" ) filltered = skill
+//       if( skill.includes( filltered ) ){
+//           if( filltered[0] === skill[0] ){
+//             answer++
+//           }
+//         }
+//     }
+//   return answer
     
     
     
