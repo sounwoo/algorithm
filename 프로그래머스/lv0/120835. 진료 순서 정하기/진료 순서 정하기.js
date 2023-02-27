eb8bef27a585eca24dfaf3de2fccb1b6deb55f95
선우 <105111888+sounwoo@users.noreply.github.com>
2023-02-27 17:31:37 +0900

    [level 0] Title: 진료 순서 정하기, Time: 0.08 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120835. 진료 순서 정하기/진료 순서 정하기.js
+++ 프로그래머스/lv0/120835. 진료 순서 정하기/진료 순서 정하기.js
@@ -1,9 +1,12 @@
 function solution(emergency) {
-  let obj = {}, temp = [...emergency];
-    for(let i =0; i <emergency.length; i++){
-      emergency.sort((a,b) => b - a )
-      obj[emergency[i]] = i + 1
-    }
+    let temp = [...emergency].sort((a,b) => b - a);
+    return emergency.map((el) => temp.indexOf(el)+1 )
   
-  return temp.map((el) => obj[el])
+//   let obj = {}, temp = [...emergency];
+//     for(let i =0; i <emergency.length; i++){
+//       emergency.sort((a,b) => b - a )
+//       obj[emergency[i]] = i + 1
+//     }
+  
+//   return temp.map((el) => obj[el])
 }
