11304a6b372fb8beba3de06cdddc9e94fe3772db
선우 <105111888+sounwoo@users.noreply.github.com>
2023-02-27 14:32:28 +0900

    [level 0] Title: 합성수 찾기, Time: 0.18 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120846. 합성수 찾기/합성수 찾기.js
+++ 프로그래머스/lv0/120846. 합성수 찾기/합성수 찾기.js
@@ -1,12 +1,12 @@
 function solution(n) {
-  let temp = [], count = 0;
+  let count = 0;
     for(let i =1; i <= n; i++){
+      let temp = []; 
       for(let j = 1; j <=n; j++){
         if(i % j === 0) temp.push(j)
       }
       if(temp.length < 3) temp = []
       else count++
-      temp = []
     }
   return count
 }
