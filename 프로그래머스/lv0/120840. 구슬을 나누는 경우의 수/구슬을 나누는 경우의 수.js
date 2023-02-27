cb53ee5b0c983ef99b1922e2d99b9704cea05b2f
선우 <105111888+sounwoo@users.noreply.github.com>
2023-02-27 19:53:39 +0900

    Update 구슬을 나누는 경우의 수.js

--- 프로그래머스/lv0/120840. 구슬을 나누는 경우의 수/구슬을 나누는 경우의 수.js
+++ 프로그래머스/lv0/120840. 구슬을 나누는 경우의 수/구슬을 나누는 경우의 수.js
@@ -1,14 +1,12 @@
+// Hint -> n!(!팩토리얼)
+// 팩토리얼 3! => 3 * 2 * 1
+// 즉, 1 ~ 자신의 숫자 까지의 곱 
 function solution(balls, share) {
-    var answer = 0;
-
-    answer = (factorial(balls)/(factorial(balls-share)*factorial(share)));
-
-    return Math.round(answer);
+    return Math.round((factorial(balls)/(factorial(balls-share)*factorial(share))));
 }
 
-function factorial(n) {
-    console.log(n)
-    var result = 1;
-    for(var i=2; i<=n; i++) result *= i;
-    return result;
+function factorial(num) {
+    let facNumber = 1;
+    for(var i=2; i<=num; i++) facNumber *= i;
+    return facNumber;
 }
