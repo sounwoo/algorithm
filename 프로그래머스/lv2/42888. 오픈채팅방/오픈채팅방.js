418de4c73ef5a05f7ceadc5385b9c30c04beb7b5
선우 <105111888+sounwoo@users.noreply.github.com>
2023-03-03 10:38:35 +0900

    Update 오픈채팅방.js

--- 프로그래머스/lv2/42888. 오픈채팅방/오픈채팅방.js
+++ 프로그래머스/lv2/42888. 오픈채팅방/오픈채팅방.js
@@ -1,17 +1,35 @@
 function solution(record) {
-    const answer = [];
-    const user = {};
-    for(let i = 0; i <record.length; i++){
-        const[action, uid, nickname] = record[i].split(" ")
-        if( nickname ) user[ uid ] = nickname
-        if( action !== "Change" )answer.push({ action, uid })
-    }
-    for(let idx in answer){
-        answer[ idx ] = user[ answer[ idx ].uid ] + (
-        answer[ idx ].action === "Enter" ? "님이 들어왔습니다.": "님이 나갔습니다."  
-        )
-    }
-    return answer
+    record = record.map( el => el.split(" "))
+    const user = record.reduce((acc,cur) =>{
+        const[action, uid, nickname] = cur
+        if ( nickname ) acc[uid] = nickname
+        return acc
+    },{})
+    return record.reduce((acc,cur) =>{
+        const[action, uid] = cur
+        if( action !== "Change"){
+            acc.push(`${ user[ uid ] }님이 `+(
+            action === "Enter" ? "들어왔습니다.":"나갔습니다."
+            ))
+        }
+        return acc
+    },[])
+    
+    
+    
+//     const answer = [];
+//     const user = {};
+//     for(let i = 0; i <record.length; i++){
+//         const[action, uid, nickname] = record[i].split(" ")
+//         if( nickname ) user[ uid ] = nickname
+//         if( action !== "Change" )answer.push({ action, uid })
+//     }
+//     for(let idx in answer){
+//         answer[ idx ] = user[ answer[ idx ].uid ] + (
+//         answer[ idx ].action === "Enter" ? "님이 들어왔습니다.": "님이 나갔습니다."  
+//         )
+//     }
+//     return answer
     
     // let obj = {}
     // record.forEach((el) => {
