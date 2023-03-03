4ea6f819ce6bb51fe7280743e1ec0de73e20e34c
선우 <105111888+sounwoo@users.noreply.github.com>
2023-03-03 10:23:07 +0900

    [level 2] Title: 오픈채팅방, Time: 142.06 ms, Memory: 83.3 MB -BaekjoonHub

--- 프로그래머스/lv2/42888. 오픈채팅방/오픈채팅방.js
+++ 프로그래머스/lv2/42888. 오픈채팅방/오픈채팅방.js
@@ -1,17 +1,29 @@
 function solution(record) {
-    let obj = {}
-    record.forEach((el) =>{
-      
-      if(el.split(" ")[2]) obj[el.split(" ")[1]] = el.split(" ")[2]
-    })
-    const result = record.map((el)=>{
-      const action = el.split(" ")
-      if(action[0] === "Enter"){
-        return `${obj[action[1]]}님이 들어왔습니다.`
-      }else if(action[0] === "Leave"){
-        return `${obj[action[1]]}님이 나갔습니다.`
-      }
-    })
-    console.log(result)
-    return result.filter((el) => el)
+    const answer = [];
+    const user = {};
+    for(let i = 0; i <record.length; i++){
+        const[action, uid, nickname] = record[i].split(" ")
+        if( nickname ) user[ uid ] = nickname
+        if( action !== "Change" )answer.push({ action, uid })
+    }
+    for(let idx in answer){
+        answer[ idx ] = user[ answer[ idx ].uid ] + (
+        answer[ idx ].action === "Enter" ? "님이 들어왔습니다.": "님이 나갔습니다."  
+        )
+    }
+    return answer
+    
+    // let obj = {}
+    // record.forEach((el) => {
+    //     if(el.split(" ")[2]) obj[el.split(" ")[1]] = el.split(" ")[2]
+    // })
+    // const result = record.map((el)=>{
+    //   const [action,user] = el.split(" ")
+    //   if(action === "Enter"){
+    //     return `${obj[user]}님이 들어왔습니다.`
+    //   }else if(action === "Leave"){
+    //     return `${obj[user]}님이 나갔습니다.`
+    //   }
+    // })
+    // return result.filter((el) => el)
 }
