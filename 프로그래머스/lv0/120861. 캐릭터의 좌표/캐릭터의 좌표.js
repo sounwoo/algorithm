426ab04553854e25133ac000beeb67dccbdfcda2
선우 <105111888+sounwoo@users.noreply.github.com>
2023-03-07 11:58:24 +0900

    [level 0] Title: 캐릭터의 좌표, Time: 0.06 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120861. 캐릭터의 좌표/캐릭터의 좌표.js
+++ 프로그래머스/lv0/120861. 캐릭터의 좌표/캐릭터의 좌표.js
@@ -1,23 +1,15 @@
 function solution(keyinput, board) {
   const answer = [0,0];
-    for(let i = 0; i < keyinput.length; i++){
-        switch (keyinput[i]){
+    for(let key of keyinput){
+        switch (key){
             case "left": 
-             answer[0] = Math.abs(answer[0] - 1)<=Math.floor(board[0]/2) 
-                    ? answer[0]-1 : answer[0]
-                break;
+             if( -answer[0] < board[0]/2 -1) answer[0]--; break;
             case "right": 
-             answer[0] = Math.abs(answer[0] + 1)<=Math.floor(board[0]/2) 
-                    ? answer[0]+1 : answer[0]
-                break;
+             if( answer[0] < board[0]/2 -1) answer[0]++; break;
             case "up": 
-             answer[1] = Math.abs(answer[1] + 1)<=Math.floor(board[1]/2) 
-                    ? answer[1]+1: answer[1]
-                break;
+             if( answer[1] < board[1]/2 -1) answer[1]++; break;
             case "down": 
-             answer[1] = Math.abs(answer[1] - 1)<=Math.floor(board[1]/2) 
-                    ? answer[1]-1 : answer[1]
-                break;
+              if( -answer[1] < board[1]/2 -1) answer[1]--; break;
         }
     }
    return answer
