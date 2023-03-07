ae875c375c07815c3fcbcc6fe081a095674ed629
선우 <105111888+sounwoo@users.noreply.github.com>
2023-03-07 11:40:43 +0900

    [level 1] Title: 약수의 개수와 덧셈, Time: 0.52 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv1/77884. 약수의 개수와 덧셈/약수의 개수와 덧셈.js
+++ 프로그래머스/lv1/77884. 약수의 개수와 덧셈/약수의 개수와 덧셈.js
@@ -1,10 +1,11 @@
 function solution(left, right) {
   let result = 0
     for(let i = left; i <= right; i++){
-      const temp = []
+      const temp = [] // count = 0
       for(let j = 1; j <= right; j++){
-        if(i % j === 0) temp.push(j)
+        if(i % j === 0) temp.push(j) // count ++
       }
+        // count % 2 === 0
       if(temp.length % 2 === 0) result += i
       else result -= i
     }
