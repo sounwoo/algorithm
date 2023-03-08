13a33cc3f1993a46df02036af8cb4cc8e82a0db3
선우 <105111888+sounwoo@users.noreply.github.com>
2023-03-08 10:25:15 +0900

    [level 2] Title: [3차] 압축, Time: 0.45 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/17684. ［3차］ 압축/［3차］ 압축.js
@@ -0,0 +1,21 @@
+function solution(msg) {
+  const dictionary = {};
+  let index = 1;
+  for(let i = 65 ; i <= 90; i++){
+    dictionary[ String.fromCharCode(i) ] = index++
+  }
+  
+  const answer = [];
+  let str = ""
+  for(let i =0 ; i < msg.length; i++){
+    str += msg[i]
+    const next = str + msg[i + 1]
+    
+    if( !dictionary[ next ] ) {
+      dictionary[ next ] = index++
+      answer.push(dictionary[ str ])
+      str = '' 
+    }
+  }
+  return answer
+}
