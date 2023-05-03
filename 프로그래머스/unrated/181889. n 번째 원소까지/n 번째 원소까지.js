2f94704d1a7cc174e4807049e6f3815b0ec75ba3
선우 <105111888+sounwoo@users.noreply.github.com>
2023-05-03 19:01:13 +0900

    [unrated] Title: n 번째 원소까지, Time: 0.05 ms, Memory: 33.3 MB -BaekjoonHub

--- 프로그래머스/unrated/181889. n 번째 원소까지/n 번째 원소까지.js
+++ 프로그래머스/unrated/181889. n 번째 원소까지/n 번째 원소까지.js
@@ -1,5 +1,6 @@
 function solution(num_list, n) {
-    return num_list.filter((el, i) => {
-        if(i < n) return el
-    })
+    return num_list.slice(num_list, n)
+    // return num_list.filter((el, i) => {
+    //     if(i < n) return el
+    // })
 }
