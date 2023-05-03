6ce2f6b0c615555e5f385b2334de34d84b2c6a9c
선우 <105111888+sounwoo@users.noreply.github.com>
2023-05-03 21:54:01 +0900

    [unrated] Title: 원소들의 곱과 합, Time: 0.07 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/unrated/181929. 원소들의 곱과 합/원소들의 곱과 합.js
+++ 프로그래머스/unrated/181929. 원소들의 곱과 합/원소들의 곱과 합.js
@@ -1,12 +1,11 @@
 function solution(num_list) {
     let qqq = num_list[0]
     let aaa = 0
-    num_list.forEach((el) => aaa += el)
-    num_list.forEach((el, i) => {
+    num_list.forEach((el,i) => {
+        aaa += el
         i++
-        if(num_list.length > i) {
-            qqq = qqq * num_list[i]
-        }
+        if(num_list.length > i) qqq *= num_list[i]
     })
+
     return qqq < aaa*aaa ? 1:0
 }
