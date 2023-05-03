b6478ecfddb9247237378c1df1a6b359f29bd559
선우 <105111888+sounwoo@users.noreply.github.com>
2023-05-03 21:28:50 +0900

    [unrated] Title: 조건에 맞게 수열 변환하기 3, Time: 2.63 ms, Memory: 43.2 MB -BaekjoonHub

--- 프로그래머스/unrated/181835. 조건에 맞게 수열 변환하기 3/조건에 맞게 수열 변환하기 3.js
+++ 프로그래머스/unrated/181835. 조건에 맞게 수열 변환하기 3/조건에 맞게 수열 변환하기 3.js
@@ -1,3 +1,4 @@
 function solution(arr, k) {
-    return k % 2 === 0 ? arr.map((el) => el+k):arr.map((el) => el*k)
+    return arr.map((el) => k % 2 ===0 ? el + k: el * k)
+    // return k % 2 === 0 ? arr.map((el) => el+k):arr.map((el) => el*k)
 }
