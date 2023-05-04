a3513f8b7091cf9f3515cc6f63cb3b18ffac3f94
선우 <105111888+sounwoo@users.noreply.github.com>
2023-05-04 19:51:54 +0900

    Update 더 크게 합치기.js

--- 프로그래머스/unrated/181939. 더 크게 합치기/더 크게 합치기.js
+++ 프로그래머스/unrated/181939. 더 크게 합치기/더 크게 합치기.js
@@ -2,4 +2,5 @@ function solution(a, b) {
     const num = String(a) + String(b)
     const num1 = String(b) + String(a)
     return Math.max(num,num1)
+    // return Math.max(`${a}${b}` , `${b}${a}`)
 }
