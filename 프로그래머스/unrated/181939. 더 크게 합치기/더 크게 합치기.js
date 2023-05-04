78046b2460793e51df39c485cb41094156fe062c
선우 <105111888+sounwoo@users.noreply.github.com>
2023-05-04 19:49:00 +0900

    [unrated] Title: 더 크게 합치기, Time: 0.03 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/unrated/181939. 더 크게 합치기/더 크게 합치기.js
+++ 프로그래머스/unrated/181939. 더 크게 합치기/더 크게 합치기.js
@@ -1,5 +1,5 @@
 function solution(a, b) {
     const num = String(a) + String(b)
     const num1 = String(b) + String(a)
-    return num > num1 ? Number(num) : Number(num1)
+    return Math.max(num,num1)
 }
