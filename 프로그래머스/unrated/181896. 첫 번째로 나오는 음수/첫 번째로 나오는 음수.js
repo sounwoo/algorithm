037a7513c42e0a3619e855db83ba8fc8d2f57290
선우 <105111888+sounwoo@users.noreply.github.com>
2023-05-04 20:53:24 +0900

    [unrated] Title: 첫 번째로 나오는 음수, Time: 0.03 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/unrated/181896. 첫 번째로 나오는 음수/첫 번째로 나오는 음수.js
+++ 프로그래머스/unrated/181896. 첫 번째로 나오는 음수/첫 번째로 나오는 음수.js
@@ -1,5 +1,7 @@
 function solution(num_list) {
-    return num_list.indexOf(num_list.filter((el) => {
-        if(0 > el) return el
-    })[0]) ?? -1
+    // return num_list.indexOf(num_list.filter((el) => {
+    //     if(0 > el) return el
+    // })[0]) ?? -1
+    
+    return num_list.findIndex((el) => 0 > el);
 }
