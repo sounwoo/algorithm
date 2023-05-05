847c217821f11911407697a28d55c10ca139f47c
선우 <105111888+sounwoo@users.noreply.github.com>
2023-05-05 18:51:30 +0900

    Update 카운트 다운.js

--- 프로그래머스/unrated/181899. 카운트 다운/카운트 다운.js
+++ 프로그래머스/unrated/181899. 카운트 다운/카운트 다운.js
@@ -1,7 +1,10 @@
 function solution(start, end) {
-    const result = [];
-    for(let i = start ; i >= end ; i-- ){
-        result.push(i)
-    }
-    return result
+//     const result = [];
+//     for(let i = start ; i >= end ; i-- ){
+//         result.push(i)
+//     }
+//     return result
+    return new Array(start - end + 1).fill(0).map((el,i)=>{
+        return el[i] = start - i
+    })
 }
