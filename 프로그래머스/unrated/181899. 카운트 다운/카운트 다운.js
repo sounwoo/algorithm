f12da18b4f1e335c007841709f5e2186d00c4ecc
선우 <105111888+sounwoo@users.noreply.github.com>
2023-05-05 18:53:12 +0900

    [unrated] Title: 카운트 다운, Time: 0.04 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/unrated/181899. 카운트 다운/카운트 다운.js
+++ 프로그래머스/unrated/181899. 카운트 다운/카운트 다운.js
@@ -1,10 +1,9 @@
 function solution(start, end) {
-//     const result = [];
-//     for(let i = start ; i >= end ; i-- ){
-//         result.push(i)
-//     }
-//     return result
-    return new Array(start - end + 1).fill(0).map((el,i)=>{
-        return el[i] = start - i
-    })
+    // const result = [];
+    // for(let i = start ; i >= end ; i-- ){
+    //     result.push(i)
+    // }
+    // return result
+    
+    return new Array(start - end + 1).fill(start).map((el,i)=> el - i)
 }
