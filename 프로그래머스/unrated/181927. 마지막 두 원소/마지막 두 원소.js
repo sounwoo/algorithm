4743a2d46558c8f0fd7271c1a7c12cf3a6733ca7
선우 <105111888+sounwoo@users.noreply.github.com>
2023-05-06 18:55:51 +0900

    [unrated] Title: 마지막 두 원소, Time: 0.03 ms, Memory: 33.6 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181927. 마지막 두 원소/마지막 두 원소.js
@@ -0,0 +1,7 @@
+function solution(num_list) {
+    const num1 = num_list[num_list.length - 1]
+    const num2 = num_list[num_list.length - 2]
+    
+    num_list.push(num1 > num2 ? num1 - num2 : num1*2)
+    return num_list
+}
