847a2c02ae631da7bf8eb36bf7e21ac30d896824
선우 <105111888+sounwoo@users.noreply.github.com>
2023-05-10 22:40:11 +0900

    Update n보다 커질 때까지 더하기.js

--- 프로그래머스/unrated/181884. n보다 커질 때까지 더하기/n보다 커질 때까지 더하기.js
+++ 프로그래머스/unrated/181884. n보다 커질 때까지 더하기/n보다 커질 때까지 더하기.js
@@ -1,5 +1,3 @@
 function solution(numbers, n) {
-    return numbers.reduce((acc, cur) => {
-       return acc <= n ? acc + cur: acc + 0
-    },0)
+    return numbers.reduce((acc, cur) => acc + (acc <= n ? cur : 0),0)
 }
