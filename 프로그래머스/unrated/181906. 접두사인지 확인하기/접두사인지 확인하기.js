b6dff1a93dbafe78c000eb8f7cc71c92991242b6
선우 <105111888+sounwoo@users.noreply.github.com>
2023-05-10 23:28:53 +0900

    Update 접두사인지 확인하기.js

--- 프로그래머스/unrated/181906. 접두사인지 확인하기/접두사인지 확인하기.js
+++ 프로그래머스/unrated/181906. 접두사인지 확인하기/접두사인지 확인하기.js
@@ -1,5 +1,7 @@
 function solution(my_string, is_prefix) {
-    let result = 0
-    my_string.split('').reduce((acc,cur) =>acc!== is_prefix ? acc += cur : result++,'')
-    return result
+    // let result = 0
+    // my_string.split('').reduce((acc,cur) =>acc!== is_prefix ? acc += cur : result++,'')
+    // return result
+    
+    return my_string.slice(0,is_prefix.length) === is_prefix ? 1 : 0
 }
