4e3fbb7786cd40580fdb98f2441c045d07d35dad
선우 <105111888+sounwoo@users.noreply.github.com>
2023-05-10 23:09:21 +0900

    [unrated] Title: n개 간격의 원소들, Time: 0.04 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/unrated/181888. n개 간격의 원소들/n개 간격의 원소들.js
+++ 프로그래머스/unrated/181888. n개 간격의 원소들/n개 간격의 원소들.js
@@ -1,13 +1,15 @@
 function solution(num_list, n) {
-    let temp = 0
-    return num_list.filter((el, i) => {
-        if (i === 0) {
-            temp ++
-            return el
-        }
-        if(temp === n ){
-            temp = 1
-            return el
-        } else temp++
-    })
+    // let temp = 0
+    // return num_list.filter((_,i) => {
+    //     if (i === 0) {
+    //         temp ++
+    //         return el
+    //     }
+    //     if(temp === n ){
+    //         temp = 1
+    //         return el
+    //     } else temp++
+    // })
+    
+    return num_list.filter((el, i) => !(i % n))
 }
