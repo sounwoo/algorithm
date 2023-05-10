25c5405ce412363d1e45911a4e876db98e654302
선우 <105111888+sounwoo@users.noreply.github.com>
2023-05-10 22:58:56 +0900

    [unrated] Title: 주사위 게임 1, Time: 0.03 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/unrated/181839. 주사위 게임 1/주사위 게임 1.js
+++ 프로그래머스/unrated/181839. 주사위 게임 1/주사위 게임 1.js
@@ -2,4 +2,5 @@ function solution(a, b) {
     if(a % 2 === 1 && b % 2 === 1) return a*a + b*b
     else if(a % 2 === 1 || b % 2 === 1) return 2 * (a + b)
     else return Math.abs(a - b)
+    // return a % 2 && b % 2 ? a * a + b * b : a % 2 || b % 2 ? 2 * (a + b) : Math.abs(a - b)
 }
