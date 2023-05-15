1b22c7809318bbe796b97b1eefe73d607455b8d4
선우 <105111888+sounwoo@users.noreply.github.com>
2023-05-15 22:38:40 +0900

    [unrated] Title: 접미사인지 확인하기, Time: 0.03 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/unrated/181908. 접미사인지 확인하기/접미사인지 확인하기.js
+++ 프로그래머스/unrated/181908. 접미사인지 확인하기/접미사인지 확인하기.js
@@ -1,3 +1,4 @@
 function solution(my_string, is_suffix) {
-    return [...my_string].map((_,i) => my_string.slice(my_string.length - i -1)).map((el) => el === is_suffix ? "1":"").join('').length ? 1 : 0
+    return my_string.endsWith(is_suffix) ? 1 : 0
+    // return [...my_string].map((_,i) => my_string.slice(my_string.length - i -1)).map((el) => el === is_suffix ? "1":"").join('').length ? 1 : 0
 }
