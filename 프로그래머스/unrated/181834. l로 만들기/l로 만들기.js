ff05c53d426b580130b2ef70d4d64405da8a53cb
선우 <105111888+sounwoo@users.noreply.github.com>
2023-05-17 23:06:44 +0900

    Update l로 만들기.js

--- 프로그래머스/unrated/181834. l로 만들기/l로 만들기.js
+++ 프로그래머스/unrated/181834. l로 만들기/l로 만들기.js
@@ -1,3 +1,4 @@
 function solution(myString) {
-    return [...myString].map((el) => el.charCodeAt() < 108 ? "l": el).join('')
+    return [...myString].reduce((acc, cur) => acc + (cur.charCodeAt() < 108 ? "l" : cur), '')
+//     return [...myString].map((el) => el.charCodeAt() < 108 ? "l": el).join('')
 }
