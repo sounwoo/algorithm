dbaf17b8f8a538deb2ea47ab75e706c637a9a341
선우 <105111888+sounwoo@users.noreply.github.com>
2023-05-20 19:36:16 +0900

    [unrated] Title: 순서 바꾸기, Time: 0.03 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/unrated/181891. 순서 바꾸기/순서 바꾸기.js
+++ 프로그래머스/unrated/181891. 순서 바꾸기/순서 바꾸기.js
@@ -1,4 +1,5 @@
 function solution(num_list, n) {
-    const temp = num_list.splice(n)
-    return temp.concat(num_list)
+    return [...num_list.slice(n), ...num_list.slice(0,n)]
+    // const temp = num_list.splice(n)
+    // return temp.concat(num_list)
 }
