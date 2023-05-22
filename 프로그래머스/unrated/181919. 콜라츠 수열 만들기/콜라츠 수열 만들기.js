ae8d9bdaf56681dc1d00322045723ca63908ecbc
선우 <105111888+sounwoo@users.noreply.github.com>
2023-05-22 23:29:51 +0900

    Update 콜라츠 수열 만들기.js

--- 프로그래머스/unrated/181919. 콜라츠 수열 만들기/콜라츠 수열 만들기.js
+++ 프로그래머스/unrated/181919. 콜라츠 수열 만들기/콜라츠 수열 만들기.js
@@ -3,7 +3,8 @@ function solution(n) {
     
     while(result.slice(-1) > 1){
         const temp = result.slice(-1)
-        temp % 2 === 0 ? result.push(temp / 2) : result.push(3 * temp + 1)
+        result.push(temp % 2 === 0 ? temp / 2 : 3 * temp + 1)
+        // temp % 2 === 0 ? result.push(temp / 2) : result.push(3 * temp + 1)
     }
     return result
 }
