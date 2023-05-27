f05cecb66777ab4d864cf27dc6272d54c763f6c1
선우 <105111888+sounwoo@users.noreply.github.com>
2023-05-27 20:36:34 +0900

    [lv3] 이중우선순위큐.js

--- 프로그래머스/lv3/42628. 이중우선순위큐/이중우선순위큐.js
+++ 프로그래머스/lv3/42628. 이중우선순위큐/이중우선순위큐.js
@@ -1,17 +1,25 @@
 function solution(operations) {
     const result = []
-    const numlist = (num) =>{
-      result.splice(result.indexOf(num),1)
-    }
+//     const numlist = (num) =>{
+//       result.splice(result.indexOf(num),1)
+//     }
+//     operations.forEach((el)=>{
+//       const [command,num] = el.split(" ")
+//       console.log(command, num,result)
+//       if(command === "I") result.push(+num)
+//       else {
+//         if(num > 0) numlist(Math.max(...result))
+//         else numlist(Math.min(...result))
+//       }
+//     })
+    const numlist = (num) => result.splice(result.indexOf(num),1)
+    
     operations.forEach((el)=>{
       const [command,num] = el.split(" ")
-      console.log(command, num,result)
       if(command === "I") result.push(+num)
-      else {
-        if(num > 0) numlist(Math.max(...result))
-        else numlist(Math.min(...result))
-      }
+      else numlist(num > 0 ? Math.max(...result): Math.min(...result)) 
     })
+    
     return result.length
     ? [Math.max(...result), Math.min(...result)]
     : [0,0]
