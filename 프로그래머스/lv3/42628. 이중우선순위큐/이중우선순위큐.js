c9daa9bdbfd58ed16f8d5da9219fbf435cd6fe2c
선우 <105111888+sounwoo@users.noreply.github.com>
2023-05-27 20:40:00 +0900

    [level 3] Title: 이중우선순위큐, Time: 0.11 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv3/42628. 이중우선순위큐/이중우선순위큐.js
+++ 프로그래머스/lv3/42628. 이중우선순위큐/이중우선순위큐.js
@@ -1,23 +1,11 @@
 function solution(operations) {
     const result = []
-//     const numlist = (num) =>{
-//       result.splice(result.indexOf(num),1)
-//     }
-//     operations.forEach((el)=>{
-//       const [command,num] = el.split(" ")
-//       console.log(command, num,result)
-//       if(command === "I") result.push(+num)
-//       else {
-//         if(num > 0) numlist(Math.max(...result))
-//         else numlist(Math.min(...result))
-//       }
-//     })
     const numlist = (num) => result.splice(result.indexOf(num),1)
     
     operations.forEach((el)=>{
       const [command,num] = el.split(" ")
       if(command === "I") result.push(+num)
-      else numlist(num > 0 ? Math.max(...result): Math.min(...result)) 
+      else numlist((num > 0 ? Math.max : Math.min)(...result))
     })
     
     return result.length
