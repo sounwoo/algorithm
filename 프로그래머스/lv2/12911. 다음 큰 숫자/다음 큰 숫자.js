56d84f1b8ab07ec93faa8877546b911ff6c334f3
선우 <105111888+sounwoo@users.noreply.github.com>
2023-06-07 13:38:40 +0900

    [level 2] Title: 다음 큰 숫자, Time: 0.06 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv2/12911. 다음 큰 숫자/다음 큰 숫자.js
+++ 프로그래머스/lv2/12911. 다음 큰 숫자/다음 큰 숫자.js
@@ -1,13 +1,9 @@
 function solution(n) {
     const oneLength = [...n.toString(2)].filter((el) => el === "1").length
-    
     let temp = 0
-    let result = 0
-    while(temp !== oneLength){
-        n++
+    while(n++){
         temp = [...n.toString(2)].filter((el) => el === "1").length
-        result = n
+        if(temp === oneLength) return n
     }
-    return result
     
 }
