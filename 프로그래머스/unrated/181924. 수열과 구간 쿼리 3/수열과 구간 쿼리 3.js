4d159daed9b4d82f98deb0bde01939ebb03e1e45
선우 <105111888+sounwoo@users.noreply.github.com>
2023-06-12 23:29:12 +0900

    [unrated] Title: 수열과 구간 쿼리 3, Time: 0.30 ms, Memory: 33.8 MB -BaekjoonHub

--- 프로그래머스/unrated/181924. 수열과 구간 쿼리 3/수열과 구간 쿼리 3.js
+++ 프로그래머스/unrated/181924. 수열과 구간 쿼리 3/수열과 구간 쿼리 3.js
@@ -1,9 +1,12 @@
 function solution(arr, queries) {
+    // queries.forEach((el) =>{
+    //     const [i,j] = el
+    //     const temp = arr[i]
+    //     arr[i] = arr[j]
+    //     arr[j] = temp
+    // })
     queries.forEach((el) =>{
-        const [i,j] = el
-        const temp = arr[i]
-        arr[i] = arr[j]
-        arr[j] = temp
+        [arr[el[0]], arr[el[1]]] = [arr[el[1]], arr[el[0]]]
     })
     return arr
 }
