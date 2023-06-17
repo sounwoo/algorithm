2f1e01d039648663f44022505b40d98cef1e8762
선우 <105111888+sounwoo@users.noreply.github.com>
2023-06-17 21:00:12 +0900

    [unrated] Title: 세로 읽기, Time: 0.15 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/unrated/181904. 세로 읽기/세로 읽기.js
+++ 프로그래머스/unrated/181904. 세로 읽기/세로 읽기.js
@@ -1,9 +1,12 @@
 function solution(my_string, m, c) {
-    let answer = ''
-    let count = 0
-    while(count !== my_string.length / m){
-        answer +=(count === 0 ? my_string[c - 1] :  my_string[c-1+(m*count)]);
-        count++
-    }
-    return answer
+    
+    return [...my_string].filter((_,i) => i % m === c-1).join('')
+    
+    // let answer = ''
+    // let count = 0
+    // while(count !== my_string.length / m){
+    //     answer +=(!count ? my_string[c - 1] :  my_string[c-1+(m*count)]);
+    //     count++
+    // }
+    // return answer
 }
