f81cd12c3aa9eba10d1472c888cd7b99c049cd51
박선우 <105111888+sounwoo@users.noreply.github.com>
2023-08-16 23:12:28 +0900

    [unrated] Title: 문자열 묶기, Time: 5.18 ms, Memory: 54.6 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181855. 문자열 묶기/문자열 묶기.js
@@ -0,0 +1,11 @@
+function solution(strArr) {
+    const dict={}
+    strArr.forEach(item => {
+        const len = item.length
+        dict[len] = dict[len] ?? []
+        dict[len].push(item)
+    })
+    
+    const values = Object.values(dict).map(a => a.length)
+    return Math.max(...values)
+}
