fbe5fbfc05608214baad92504ffe600339bacb5b
박선우 <105111888+sounwoo@users.noreply.github.com>
2023-09-04 17:22:36 +0900

    [Bronze V] Title: 아스키 코드, Time: 124 ms, Memory: 9336 KB -BaekjoonHub

---
+++ 백준/Bronze/11654. 아스키 코드/아스키 코드.js
@@ -0,0 +1,12 @@
+const fs = require('fs');
+const input = fs
+    .readFileSync(process.platform === 'linux' ? '/dev/stdin' : 'input.txt')
+    .toString()
+    .trim()
+    .split('\n');
+
+function solution() {
+    console.log(input[0].charCodeAt());
+}
+
+solution();
