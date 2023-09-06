91613df98c11f232a162ab8584ab66b47df61f4b
박선우 <105111888+sounwoo@users.noreply.github.com>
2023-09-06 15:03:44 +0900

    [Bronze V] Title: 문자와 문자열, Time: 176 ms, Memory: 9324 KB -BaekjoonHub

---
+++ 백준/Bronze/27866. 문자와 문자열/문자와 문자열.js
@@ -0,0 +1,13 @@
+const fs = require('fs');
+const input = fs
+    .readFileSync(process.platform === 'linux' ? '/dev/stdin' : 'input.txt')
+    .toString()
+    .trim()
+    .split('\n');
+
+function solution() {
+    const [word, i] = input;
+    console.log(word[i - 1]);
+}
+
+solution();
