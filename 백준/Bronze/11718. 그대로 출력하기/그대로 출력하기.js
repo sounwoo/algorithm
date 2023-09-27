c3e7d6646267ddbb84c79f20f0d6187363ef6695
박선우 <105111888+sounwoo@users.noreply.github.com>
2023-09-27 13:55:36 +0900

    [Bronze V] Title: 그대로 출력하기, Time: 156 ms, Memory: 9492 KB -BaekjoonHub

---
+++ 백준/Bronze/11718. 그대로 출력하기/그대로 출력하기.js
@@ -0,0 +1,12 @@
+const fs = require('fs');
+const input = fs
+    .readFileSync(process.platform === 'linux' ? '/dev/stdin' : 'input.txt')
+    .toString()
+    .trim()
+    .split('\n');
+
+function solution() {
+    input.forEach((el) => console.log(el))
+}
+
+solution();
