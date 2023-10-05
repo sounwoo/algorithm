9a5e27de47eb4e5d46a37dcf54b88a34802fbe2e
박선우 <105111888+sounwoo@users.noreply.github.com>
2023-10-06 00:27:37 +0900

    [Unrated] Title: 더하기, Time: 112 ms, Memory: 9324 KB -BaekjoonHub

---
+++ 백준/Unrated/14918. 더하기/더하기.js
@@ -0,0 +1,13 @@
+const fs = require('fs');
+const input = fs
+    .readFileSync(process.platform === 'linux' ? '/dev/stdin' : 'input.txt')
+    .toString()
+    .trim()
+    .split('\n');
+
+function solution() {
+    const [A, B] = input[0].split(' ');
+    console.log(+A + +B);
+}
+
+solution();
