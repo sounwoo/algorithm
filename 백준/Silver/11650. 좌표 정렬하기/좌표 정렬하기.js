4ed913bffe900b014dbedd2838b4f497f588a805
박선우 <105111888+sounwoo@users.noreply.github.com>
2023-10-16 16:13:33 +0900

    [Silver V] Title: 좌표 정렬하기, Time: 504 ms, Memory: 60872 KB -BaekjoonHub

---
+++ 백준/Silver/11650. 좌표 정렬하기/좌표 정렬하기.js
@@ -0,0 +1,18 @@
+const fs = require('fs');
+const input = fs
+    .readFileSync(process.platform === 'linux' ? '/dev/stdin' : 'input.txt')
+    .toString()
+    .trim()
+    .split('\n');
+
+function solution() {
+    const [N, ...arr] = input;
+    const newArr = arr.map((el) => el.split(' ').map((els) => +els));
+    let result = '';
+    newArr
+        .sort((a, b) => (a[0] !== b[0] ? a[0] - b[0] : a[1] - b[1]))
+        .forEach((el) => (result += `${el[0]} ${el[1]}\n`));
+    console.log(result);
+}
+
+solution();
