a7025e47a9bfd94f2e1bcc14b6a30be09cf2a1bb
박선우 <105111888+sounwoo@users.noreply.github.com>
2023-10-20 17:49:51 +0900

    [Bronze II] Title: 분산처리, Time: 4460 ms, Memory: 9696 KB -BaekjoonHub

---
+++ 백준/Bronze/1009. 분산처리/분산처리.js
@@ -0,0 +1,24 @@
+const fs = require('fs');
+const input = fs
+    .readFileSync(process.platform === 'linux' ? '/dev/stdin' : 'input.txt')
+    .toString()
+    .trim()
+    .split('\n');
+
+function solution() {
+    const [N, ...arr] = input;
+
+    const result = arr.map((el) => {
+        const [a, b] = el.split(' ');
+        let pow = 1;
+
+        for (let j = 0; j < b; j++) {
+            pow = (pow * a) % 10;
+        }
+
+        return pow ===0 ? 10 : pow;
+    });
+    console.log(result.join('\n'));
+}
+
+solution();
