ab4a9541e143f8c1514f3cebab9f59c5d6b013b7
박선우 <105111888+sounwoo@users.noreply.github.com>
2023-11-24 22:57:35 +0900

    [Bronze II] Title: 피보나치 수, Time: 128 ms, Memory: 9588 KB -BaekjoonHub

---
+++ 백준/Bronze/2747. 피보나치 수/피보나치 수.js
@@ -0,0 +1,15 @@
+const fs = require('fs');
+const input = fs
+    .readFileSync(process.platform === 'linux' ? '/dev/stdin' : 'input.txt')
+    .toString()
+    .trim();
+
+function solution(n) {
+    let fibo = [0, 1];
+    for (let i = 2; i <= n; i++) {
+        fibo[i] = fibo[i - 1] + fibo[i - 2];
+    }
+    return fibo[n];
+}
+
+console.log(solution(+input));
